4ead6dc5d8d59c3e1b37bd53dd4af0726f0b7357
008056848 <008056848@coyote.csusb.edu>
2022-11-21 17:16:34 -0800

    pet feeding and increment.
    increment/decrementing the pet feed fruits.

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 
 public class Drag : MonoBehaviour
@@ -15,8 +19,31 @@ public class Drag : MonoBehaviour
     public GameObject watermelon;
     public GameObject guava;
 
-    public void DragHandler(BaseEventData data)
+    public GameObject appleaxis;
+    public GameObject orangeaxis;
+    public GameObject melonaxis;
+    public GameObject guavaaxis;
+
+    public TMP_Text applecount;
+    public TMP_Text meloncount;
+    public TMP_Text orangecount;
+    public TMP_Text guavacount;
+
+
+    public void Start()
     {
+       // startingObject = GameObject.Find("Starting"); //Find it here, or drag it into the hierarchy
+
+        appleaxis = GameObject.Find("apple");
+        melonaxis = GameObject.Find("watermelon");
+        guavaaxis = GameObject.Find("guava");
+        orangeaxis = GameObject.Find("orange");
+
+    }
+
+public void DragHandler(BaseEventData data)
+    {
+
         PointerEventData pointerData = (PointerEventData)data;
 
         Vector2 position;
@@ -27,69 +54,103 @@ public class Drag : MonoBehaviour
             out position);
 
         transform.position = canvas.transform.TransformPoint(position);
-    
 
-        float appleX = (GameObject.Find("apple").transform.position.x);
-        float appleY = (GameObject.Find("apple").transform.position.y);
+        Debug.Log("test1");
+        //appleaxis = GameObject.Find("apple");
+        float appleX = appleaxis.transform.position.x;
+        float appleY = appleaxis.transform.position.y;
+        //float appleY = (GameObject.Find("apple").transform.position.y);
+        Debug.Log("test2");
+        //orangeaxis = GameObject.Find("orange");
+      //  float orangeX = (GameObject.Find("orange").transform.position.x);
+      //  float orangeY = (GameObject.Find("orange").transform.position.y);
+        float orangeX = orangeaxis.transform.position.x;
+        float orangeY = orangeaxis.transform.position.y;
+        Debug.Log("test3");
+      //  float melonX = (GameObject.Find("watermelon").transform.position.x);
+      //  float melonY = (GameObject.Find("watermelon").transform.position.y);
 
-        float orangeX = (GameObject.Find("orange").transform.position.x);
-        float orangeY = (GameObject.Find("orange").transform.position.y);
+        float melonX = melonaxis.transform.position.x;
+        float melonY = melonaxis.transform.position.y;
 
-        float melonX= (GameObject.Find("watermelon").transform.position.x);
-        float melonY = (GameObject.Find("watermelon").transform.position.y);
+        Debug.Log("test4");
+      //  float guavaX = (GameObject.Find("guava").transform.position.x);
+      //  float guavaY = (GameObject.Find("guava").transform.position.y);
 
-        float guavaX = (GameObject.Find("guava").transform.position.x);
-        float guavaY = (GameObject.Find("guava").transform.position.y);
-
-
+        float guavaX = guavaaxis.transform.position.x;
+        float guavaY = guavaaxis.transform.position.y;
 
 
         float catXmin = (GameObject.Find("Cateating").transform.position.x);
         float catYmin = (GameObject.Find("Cateating").transform.position.y);
-        float catXmax = catXmin + 50;
-        catYmin = catYmin + 50;
+        float catXmax = catXmin + 100;
+        catYmin = catYmin + 100;
         float catYmax = catYmin - 100; 
         Debug.Log(transform.position);
+        //if (clickControll.orangebingoinc == 0)
+        //{
+
         if (orangeX > catXmin && orangeX < catXmax && orangeY < catYmin && orangeY > catYmax)
-        {
+            {
+               
             orange.SetActive(false);
-
-            //Fruitinc.applecount = (clickControll.applebingoinc - 1).ToString();
-            Debug.Log("hello eat");
-
-           // fruit.SetActive(true);
-
-        }
-        else if (appleX > catXmin && appleX < catXmax && appleY < catYmin && appleY > catYmax)
-        {
-            apple.SetActive(false);
-
-          //  Fruitinc.applecount = (clickControll.applebingoinc - 1).ToString();
-            Debug.Log("hello eat");
+            //Fruitinc.orangecount = (clickControll.orangebingoinc - 1).ToString();
+            
+            orangecount.text  = (clickControll.orangebingoinc - 1).ToString();
+                
+                //Debug.Log("hello ea apple"+ orangecountval);
 
             // fruit.SetActive(true);
 
         }
-        else if (melonX > catXmin && melonX < catXmax && melonY < catYmin && melonY > catYmax)
-        {
-            watermelon.SetActive(false);
+         //}
+          //  if (clickControll.applebingoinc != 0)
+        //{
+            if (appleX > catXmin && appleX < catXmax && appleY < catYmin && appleY > catYmax)
+            {
+                apple.SetActive(false);
+                //  int y = clickControll.applebingoinc - 1;
+                applecount.text = (clickControll.applebingoinc - 1).ToString();
+                //Fruitinc.acount = (clickControll.applebingoinc - 1);
+                Debug.Log("hello eat");
 
-            //Fruitinc.applecount = (clickControll.applebingoinc - 1).ToString();
-            Debug.Log("hello eat");
 
-            // fruit.SetActive(true);
+                // fruit.SetActive(true);
 
-        }
-        else if (guavaX > catXmin && guavaX < catXmax && guavaY < catYmin && guavaY > catYmax)
-        {
-            guava.SetActive(false);
+            }
+        //}
+        //if (clickControll.melonbingoinc != 0)
+        //{
+           
 
-            //Fruitinc.applecount = (clickControll.applebingoinc - 1).ToString();
-            Debug.Log("hello eat");
+            if (melonX > catXmin && melonX < catXmax && melonY < catYmin && melonY > catYmax)
+            {
+                watermelon.SetActive(false);
+                //int z = clickControll.melonbingoinc - 1;
 
-            // fruit.SetActive(true);
 
-        }
+                meloncount.text = (clickControll.melonbingoinc - 1).ToString();
+                Debug.Log("hello eat");
+
+                // fruit.SetActive(true);
+
+            }
+       // }
+        //if (clickControll.guavabingoinc != 0)
+        //{
+            if (guavaX > catXmin && guavaX < catXmax && guavaY < catYmin && guavaY > catYmax)
+            {
+                guava.SetActive(false);
+                //int s = clickControll.guavabingoinc - 1;
+
+               guavacount.text = (clickControll.guavabingoinc - 1).ToString();
+                Debug.Log("hello eat");
+
+                // fruit.SetActive(true);
+
+            }
+       // }
+
     }
 }
 /*using System.Collections;
--- Assets/Scripts/Fruitinc.cs
+++ Assets/Scripts/Fruitinc.cs
@@ -26,36 +26,66 @@ public class Fruitinc : MonoBehaviour
 	public TMP_Text orangecount;
 	public TMP_Text guavacount;
 	public int fruit_counter = 0;
+	/*public int count1;
+	public int count2;
+	public int count3;
+	public int count4;
+	public int acount;*/
+
 	void Start()
 	{
 		//Texture2D myTexture;
 		//RawImage.enabled = false;
-
+		
 		if (clickControll.over == true)
 		{
 			// load texture from resource folder			
 			//myTexture = Resources.Load("apple-removebg-preview") as Texture2D;
 			//GameObject apple = GameObject.Find("apple");
 			//apple.GetComponent<apple>().texture = myTexture;
-			applecount.text = clickControll.applebingoinc.ToString();
+			//count1 = clickControll.applebingoinc;
+			//applecount.text = count1.ToString();
 			
+			applecount.text= clickControll.applebingoinc.ToString();
+
+
 		}
 		if (clickControll.over1 == true)
         {
+			//count2 = clickControll.guavabingoinc;
+			//guavacount.text = count2.ToString();
 			guavacount.text = clickControll.guavabingoinc.ToString();
 		}
 		if (clickControll.over2 == true)
 		{
+			//count3 = clickControll.melonbingoinc;
+			//meloncount.text = count3.ToString();
 			meloncount.text = clickControll.melonbingoinc.ToString();
 		}
 		if (clickControll.over3 == true)
 		{
+			//count4 = clickControll.orangebingoinc;
+			//orangecount.text = count4.ToString();
 			orangecount.text = clickControll.orangebingoinc.ToString();
-
 		}
 
-		
-		
+		/*if(Drag.x!= count4)
+        {
+			orangecount.text = Drag.x.ToString();
+		}
+		if (Drag.y != count1)
+		{
+			applecount.text = Drag.y.ToString();
+		}
+		if (Drag.z != count3)
+		{
+			meloncount.text = Drag.z.ToString();
+		}
+		if (Drag.s != count2)
+		{
+			guavacount.text = Drag.s.ToString();
+		}
+		*/
 		//      else
 		//      {
 		//	applecount.text = fruit_counter.ToString();
